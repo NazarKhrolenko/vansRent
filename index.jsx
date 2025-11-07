3fc9cc8ec739534ac49663fbd080e3287a97c946
Nazar <nazarkhrolenko@gmail.com>
2025-11-07 12:57:09 +0100

    added action functionality for form

--- index.jsx
+++ index.jsx
@@ -1,30 +1,99 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  redirect
+  Link
 } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans, { loader as vansLoader } from "./pages/Vans/Vans"
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans, { loader as hostVansLoader} from "./pages/Host/HostVans"
+import HostVanDetail, { loader as hostVanDetailLoader } from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import NotFound from "./pages/NotFound"
+import Login, { loader as loginLoader, action as loginAction } from "./pages/Login"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+import Error from "./components/Error"
 import { requireAuth } from "./utils"
 
-import Layout from "./Layout"
-import AuthRequired from "./AuthRequired"
+import "./server"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
     <Route
-      index
-      element={<h1>Home page</h1>}
+      path="login"
+      element={<Login />}
+      loader={loginLoader}
+      action={loginAction}
     />
     <Route
-      path="protected"
-      element={<h1>Super secret info here</h1>}
-      loader={async () => await requireAuth()}
+      path="vans"
+      element={<Vans />}
+      errorElement={<Error />}
+      loader={vansLoader}
     />
-    <Route path="login" element={<h1>Login page goes here</h1>} />
+    <Route 
+      path="vans/:id" 
+      element={<VanDetail />} 
+      loader={vanDetailLoader}
+    />
 
+    <Route path="host" element={<HostLayout />}>
+      <Route
+        index
+        element={<Dashboard />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="income"
+        element={<Income />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="reviews"
+        element={<Reviews />}
+        loader={async () => await requireAuth()}
+      />
+      <Route
+        path="vans"
+        element={<HostVans />}
+        loader={hostVansLoader}
+      />
+      <Route
+        path="vans/:id"
+        element={<HostVanDetail />}
+        loader={hostVanDetailLoader}
+      >
+        <Route
+          index
+          element={<HostVanInfo />}
+          loader={async () => await requireAuth()}
+        />
+        <Route
+          path="pricing"
+          element={<HostVanPricing />}
+          loader={async () => await requireAuth()}
+        />
+        <Route
+          path="photos"
+          element={<HostVanPhotos />}
+          loader={async () => await requireAuth()}
+        />
+      </Route>
+    </Route>
+    <Route path="*" element={<NotFound />} />
   </Route>
 ))
 
@@ -34,4 +103,6 @@ function App() {
   )
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
